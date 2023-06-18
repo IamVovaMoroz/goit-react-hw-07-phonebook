c052aa0a372df73a7ca367a4e00420ab73f04721
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-06-18 19:45:59 +0200

    ok

--- src/redux/contactSelector.js
+++ src/redux/contactSelector.js
@@ -1,6 +1,6 @@
 //  возвращает массив контактов из состояния Redux.  получения списка контактов.
 export const getContacts = state => state.contacts.items;
-// возвращает значение фильтра , проверяет, является ли filterValue строкой , если не строка то filterValue = ''
+// возвращает значение фильтра , проверяет, является ли filterValue строкой, если не строка то filterValue = ''
 export const getFilteredContact = state => {
   const filterValue = state.filter.filterValue;
   if (typeof filterValue !== 'string') {
